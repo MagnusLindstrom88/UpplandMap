578ffbf577ad6e9d4281207209d146112f7fbcc6
Martin Warström <martin@warstrom.se>
2015-12-10 12:56:37 +0100

    La till en ny karta ovan det gamla

--- UM/server.R
+++ UM/server.R
@@ -2,25 +2,14 @@ library(shiny)
 library(leaflet)
 library(magrittr)
 
-# shinyServer(
-#   function(input, output) {
-#     
-#     
-#     map<-leaflet() %>% setView(lat =59.5167, lng =17.9167, zoom = 12)
-#     map %>% addTiles()
-#     
-#     output$myMap <- renderLeaflet(map)
-#     
-#   }
-# )
-
 
 shinyServer(function(input, output, session) {
   
   output$map <- renderLeaflet({
     leaflet() %>%
       addTiles()%>%
-      setView(lng = 17.9167, lat = 59.5167, zoom = 13)
+      setView(lng = 17.9167, lat = 59.5167, zoom = 13) %>%
+      addProviderTiles("CartoDB.Positron")
   })
   
 })
