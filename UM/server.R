514af2921a40f68dabec4c317e4cb50e42159f2e
MagnusLindstrom88 <magnus.rolf.lindstrom@gmail.com>
2015-12-17 23:37:55 +0100

    La till filer för att rita ut om olika områedna i UV på en karta

--- UM/server.R
+++ UM/server.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(leaflet)
 library(magrittr)
+library(sp)
 
 
 shinyServer(function(input, output, session) {
@@ -8,8 +9,12 @@ shinyServer(function(input, output, session) {
   output$map <- renderLeaflet({
     leaflet() %>%
       addTiles()%>%
-      setView(lng = 17.9167, lat = 59.5167, zoom = 13) %>%
+      addPolygons(data=uv84)%>%
+      # setView(lng = 17.9167, lat = 59.5167, zoom = 13) %>%
       addProviderTiles("CartoDB.Positron")
+     
   })
+
+
   
 })
--- UM/ui.R
+++ UM/ui.R
@@ -1,16 +1,30 @@
 library(shiny)
 library(leaflet)
 library(magrittr)
+library(sp)
 
 # 
 # shinyUI(fluidPage(leafletOutput("myMap")))
 #   
 
 shinyUI(fluidPage(
-  titlePanel("title panel"),
-                   
+  titlePanel("Title panel"),
+    
+  sidebarLayout(
+    sidebarPanel(
+      helpText("Välj ett alternativ"),
+      
+      selectInput("var", 
+                  label = "Choose a variable to display",
+                  choices = c("1", "2",
+                              "3", "4"),
+                  selected = "1")
+    ),
          
 mainPanel(leafletOutput("map")
+          
+          
 
 ))
+)
 )
